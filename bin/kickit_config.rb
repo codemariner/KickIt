97aff276c08be8c2364fc182f8bf0390b9023dcd
Scott Sayles <scott.sayles@gmail.com>
2011-11-19 12:44:53 -0500

    setting config

--- bin/kickit_config.rb
+++ bin/kickit_config.rb
@@ -1,8 +1,8 @@
 require 'lib/kickit'
 
 Kickit::Config.new do |config|
-  config.developerKey = '2d7d9f42'
-  config.as = 193980
+  config.developerKey = '9999999'
+  config.as = 999999
   config.admin_username = 'ssayles'
   config.rest_base_uri = 'http://api.kickapps.com/rest'
   config.feed_url = 'http://cdnserve.a-feed.com/service/getFeed2.kickAction'
